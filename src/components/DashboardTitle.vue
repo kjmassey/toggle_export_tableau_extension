6278e55854036b6287b3369fb16647a6b2227290
Kyle Massey <kyle.massey@gmail.com>
2022-09-22 00:09:34 -0400

    - Prevent db name wrap - truncate db name

--- src/components/DashboardTitle.vue
+++ src/components/DashboardTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-space-between align-center">
-    <h3 class="py-2">{{ dbName }}</h3>
+    <h3 class="py-2 text-no-wrap text-truncate">{{ dbName }}</h3>
     <div>
       <v-icon v-on:click="showAbout()" color="#4E79A7"
         >mdi-information-outline</v-icon
