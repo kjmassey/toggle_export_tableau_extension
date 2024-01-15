9d1c1cda641dc72fd1f4e94a09e36fdf661c0b3d
Kyle Massey <kyle.massey@gmail.com>
2024-01-15 10:09:32 -0500

    WIP unused

---
+++ src/components/GetSelectedMarks.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-btn>Get Selected Marks</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { ws: Object, db: Object },
+};
+</script>
+
+<style></style>
